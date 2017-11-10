e78f92fdeee36259826c0a5ece6c19546d5b8231
James <james.patrick.fulford@gmail.com>
2017-11-09 20:11:00 -0500

    Added basic DOM manipulation examples

---
+++ Basic Javascript/6 DOM Manipulation/dom_manipulate.js
@@ -0,0 +1,27 @@
+
+// One li
+var tag = document.getElementById("highlight");
+
+
+// Two li's
+var bolded = document.getElementsByClassName("bolded");
+
+
+// All 3 li's
+var li = document.getElementsByTagName("li");
+
+// All 2 h1's
+var h1 = document.getElementsByTagName("h1");
+
+// Title tag - still a NodeList
+var title = document.getElementsByTagName("title")
+
+
+// One li
+// Just first element that matches
+var highlight = document.querySelector("#highlight");
+
+
+// All lis
+// NodeList
+var lis = document.querySelectorAll("li")
