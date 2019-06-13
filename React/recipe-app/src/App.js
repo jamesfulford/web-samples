2cbfbca0ca2a3f66291cbedca3202d4856df8fe9
James Fulford <james.fulford@outlook.com>
2019-06-12 20:06:55 -0400

    feat(react): Recipe App: Added Recipe component

--- React/recipe-app/src/App.js
+++ React/recipe-app/src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import Recipe from './Recipe';
 
 function App() {
+  const recipes = [
+    {
+      img: logo,
+      instructions: "Mix well",
+      ingredients: ['a', 'b'],
+      title: 'Test',
+    }, {
+      img: logo,
+      instructions: "Mix well",
+      ingredients: ['a', 'b'],
+      title: 'Test',
+    }, {
+      img: logo,
+      instructions: "Mix well",
+      ingredients: ['a', 'b'],
+      title: 'Test',
+    },
+  ];
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        recipes.map(r => (
+          <Recipe {...r}></Recipe>
+        ))
+      }
     </div>
   );
 }
---
+++ React/recipe-app/src/Recipe.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './Recipe.css';
+import PropTypes from 'prop-types';
+
+export default class Recipe extends Component {
+    static propTypes = {
+        title: PropTypes.string.isRequired,
+        instructions: PropTypes.string.isRequired,
+        ingredients: PropTypes.arrayOf(PropTypes.string),
+        img: PropTypes.string,
+    };
+    static defaultProps = {
+        ingredients: [],
+    };
+
+    render () {
+        const { title, img, ingredients, instructions } = this.props;
+        return (
+            <div className="recipe-card">
+                <div className="recipe-card-img">
+                    <img src={img} alt={title} />
+                </div>
+                <div className="recipe-card-content">
+                    <h3 className="recipe-title">{title}</h3>
+                    <h4>Ingredients</h4>
+                    <ul>
+                        {ingredients.map((ing, i) => (
+                            <li key={ing}>{ing}</li>
+                        ))}
+                    </ul>
+                    <h4>Instructions</h4>
+                    <p>
+                        {instructions}
+                    </p>
+                </div>
+            </div>
+        )
+    }
+}
