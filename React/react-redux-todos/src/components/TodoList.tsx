bf95abe07a3f4050c46be1c54a324a6e29986516
James Fulford <james.fulford@outlook.com>
2019-06-17 23:50:01 -0400

    feat(react): Redux Todos: dispatching add todos

--- React/react-redux-todos/src/components/TodoList.tsx
+++ React/react-redux-todos/src/components/TodoList.tsx
@@ -4,16 +4,40 @@ import { Todo } from "../todo.types";
 
 import { connect } from "react-redux";
 import { ReducerState } from "../reducers/todos";
+import { addTodo } from "../actions/todos";
 
-const TodoList: FunctionComponent<{ todos: Todo[] }> = ({ todos }) => {
+const TodoList: FunctionComponent<{ todos: Todo[]; dispatch: Function }> = ({
+  todos,
+  dispatch
+}) => {
+  const [text, setText] = useState<string>("");
   return (
-    <div>
-      {todos.map((todo: Todo) => {
-        return (
-          <TodoItem key={todo.id} name={todo.name} completed={todo.completed} />
-        );
-      })}
-    </div>
+    <>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          dispatch(addTodo({ name: text, completed: false }));
+          setText("");
+        }}
+      >
+        <input
+          type="text"
+          onChange={e => setText(e.target.value)}
+          value={text}
+        />
+      </form>
+      <div>
+        {todos.map((todo: Todo) => {
+          return (
+            <TodoItem
+              key={todo.id}
+              name={todo.name}
+              completed={todo.completed}
+            />
+          );
+        })}
+      </div>
+    </>
   );
 };
 
