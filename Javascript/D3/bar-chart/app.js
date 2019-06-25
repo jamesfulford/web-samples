4d8a31c2c4f022b72194b35a28fd78eb230205c4
James Fulford <james.fulford@outlook.com>
2019-06-24 21:03:14 -0400

    refactor(d3): Bar chart: removed hardcoded scaling

--- Javascript/D3/bar-chart/app.js
+++ Javascript/D3/bar-chart/app.js
@@ -1,9 +1,21 @@
-var minYear = birthData[0].year;
-var maxYear = birthData[birthData.length - 1].year;
+var minYear = d3.min(birthData, d => d.year);
+var maxYear = d3.max(birthData, d => d.year);
+var maxBirths = d3.max(birthData, d => d.births);
 
 var width = 600;
 var height = 600;
 var barWidth = 25;
+
+var yScale = d3.scaleLinear()
+    .domain([0, maxBirths])
+    .range([height, 0]); // flip upside-down so 'y' means bottom left
+
+function yScaleByBirth(d) {
+    return yScale(d.births);
+}
+function yScaleByBirth_height(d) {
+    return height - yScaleByBirth(d);
+}
 
 d3
     .select('input')
@@ -16,8 +28,8 @@ d3.select('input')
         var year = +d3.event.target.value;
         d3.selectAll('rect')
             .data(birthData.filter(d => d.year === year))
-                .attr('height', d => d.births / 2.5e6 * height)
-                .attr('y', d => height - (d.births / 2.5e6 * height))
+                .attr('height', yScaleByBirth_height)
+                .attr('y', yScaleByBirth)
     });
 
 d3
@@ -29,6 +41,6 @@ d3
     .enter()
     .append('rect')
         .attr('width', barWidth)
-        .attr('height', d => (d.births / 2.5e6) * height)
-        .attr('y', d => height - (d.births / 2.5e6 * height))
+        .attr('height', yScaleByBirth_height)
+        .attr('y', yScaleByBirth)
         .attr('x', (d, i) => i * (barWidth + 10))
