7a10bfab96314605b65dbf2e7d0f69e59011a19f
James Fulford <james.fulford@outlook.com>
2019-07-21 19:02:02 -0400

    feat(d3): Force Directed Graph

--- D3/force-directed-graph/app.js
+++ D3/force-directed-graph/app.js
@@ -10,9 +10,27 @@ var nodes = [
   { color: "purple", size: 30 }
 ];
 
+const links = [
+  { source: 'red', target: 'orange' },
+  { source: 'orange', target: 'yellow' },
+  { source: 'yellow', target: 'green' },
+  { source: 'green', target: 'blue' },
+  { source: 'blue', target: 'purple' },
+  { source: 'purple', target: 'red' },
+  { source: 'green', target: 'red' },
+];
+
 var svg = d3.select("svg")
               .attr("width", width)
               .attr("height", height);
+
+const linkSelection = svg
+  .selectAll('line')
+  .data(links)
+  .enter()
+  .append('line')
+    .attr('stroke', 'black')
+    .attr('stroke-width', 1)
 
 var nodeSelection = svg
                       .selectAll("circle")
@@ -21,3 +39,24 @@ var nodeSelection = svg
                       .append("circle")
                         .attr("r", d => d.size)
                         .attr("fill", d => d.color);
+
+var simulation = d3.forceSimulation(nodes);
+
+simulation
+  .force('center', d3.forceCenter(width / 2, height / 2))
+  .force('nodes', d3.forceManyBody().strength(-30))
+  .force('links', d3.forceLink(links)
+    .id(d => d.color)
+    .distance(300)
+  )
+  .on('tick', () => {
+    nodeSelection
+      .attr('cx', d => d.x)
+      .attr('cy', d => d.y)
+
+    linkSelection
+      .attr("x1", d => d.source.x)
+      .attr("y1", d => d.source.y)
+      .attr("x2", d => d.target.x)
+      .attr("y2", d => d.target.y);
+  })
