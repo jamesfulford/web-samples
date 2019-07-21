09d70c8a2348720dd8c2e33fc12f7896d340d0d5
James Fulford <james.fulford@outlook.com>
2019-07-21 16:20:20 -0400

    feat(d3): Bar chart: adding transitions

--- Javascript/D3/bar-chart/app.js
+++ Javascript/D3/bar-chart/app.js
@@ -26,10 +26,25 @@ d3
 d3.select('input')
     .on('input', () => {
         var year = +d3.event.target.value;
-        d3.selectAll('rect')
-            .data(birthData.filter(d => d.year === year))
-                .attr('height', yScaleByBirth_height)
-                .attr('y', yScaleByBirth)
+        d3.selectAll("rect")
+          .data(birthData.filter(d => d.year === year))
+          .transition()
+          .duration(100)
+          .delay((d, i) => i * 25)
+          .ease(d3.easeLinear)
+          .attr("height", yScaleByBirth_height)
+          .attr("y", yScaleByBirth)
+          .on("start", (_d, i) => {
+            if (i === 0) {
+              d3.select(".title").text(`Updating to ${year}...`);
+            }
+          })
+          .on("end", (_d, i, nodes) => {
+            if (i === nodes.length - 1) {
+              d3.select(".title").text(`Birth Data for ${year}`);
+            }
+          });
+
     });
 
 d3
@@ -44,3 +59,13 @@ d3
         .attr('height', yScaleByBirth_height)
         .attr('y', yScaleByBirth)
         .attr('x', (d, i) => i * (barWidth + 10))
+
+d3.select('svg')
+    .append('text')
+    .classed('title', true)
+    .text(`Birth Data in ${minYear}`)
+    .attr('x', width / 2)
+    .attr('y', 30)
+    .style('text-anchor', 'middle')
+    .style('font-size', '2em');
+
